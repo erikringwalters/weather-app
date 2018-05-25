e98d3b3dece8b9c879c76d5c108100c5e8ba61a0
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-05-25 15:14:46 -0700

    cities component is now retrieving data from service

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { City } from '../city';
 import { CITIES } from '../mock-cities'
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
   styleUrls: ['./cities.component.css']
 })
+
 export class CitiesComponent implements OnInit {
 
   cities = CITIES;
 
   selectedCity: City;
 
-  constructor() { }
+  constructor(private cityService: CityService) { }
 
   ngOnInit() {
-    this.selectedCity = this.cities[1];
+    //Selects random city out of array
+    this.selectedCity = this.cities[Math.floor(Math.random() * this.cities.length)];
+
+    this.getCities();
   }
 
   onSelect(city: City): void {
     this.selectedCity = city;
+  }
+
+  getCities(): void {
+    this.cities = this.cityService.getCities();
   }
 }
