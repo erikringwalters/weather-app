9b76a0c083ea5599b1b143c8301182293615b5fe
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-05-25 12:51:56 -0700

    displaying list of cities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddCityComponent } from './add-city/add-city.component';
+import { ListCitiesComponent } from './list-cities/list-cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCityComponent,
+    ListCitiesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-cities.ts
@@ -0,0 +1,10 @@
+import { City } from './city';
+
+export const CITIES: City[] = [
+  { id: 11, name: 'San Diego, California' },
+  { id: 12, name: 'Los Angeles, California' },
+  { id: 13, name: 'Dallas, Texas' },
+  { id: 14, name: 'Portland, Oregon' },
+  { id: 15, name: 'Cupertino, California' },
+  { id: 16, name: 'New York, New York' }
+];
